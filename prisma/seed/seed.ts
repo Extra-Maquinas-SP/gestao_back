894d8a6bd21792deb622c492712693582cf007dd
gumartins77 <gugumartin77@gmail.com>
2023-09-26 13:07:22 -0300

    reafctor seed

--- prisma/seed/seed.ts
+++ prisma/seed/seed.ts
@@ -8,7 +8,7 @@ async function main() {
     await prisma.$queryRaw(
       Prisma.sql`INSERT INTO usuarios (id, nome, email, senha, telefone, tipo)
       VALUES
-        (4, 'Gustavo Martins', 'tisp@extramaquinassp.com.br', '$2b$10$t2T0vCYkdb3mBzUh9Zw8Tui9X8v4He/4ET/BNCfR.b4ug0GAcKykG', '+5519992599246', 'gestor'),
+        (1, 'Gustavo Martins', 'tisp@extramaquinassp.com.br', '$2b$10$t2T0vCYkdb3mBzUh9Zw8Tui9X8v4He/4ET/BNCfR.b4ug0GAcKykG', '+5519992599246', 'gestor'),
         (2, 'Julia Téles', 'dev1@extramaquinassp.com.br', '$2b$10$t2T0vCYkdb3mBzUh9Zw8Tui9X8v4He/4ET/BNCfR.b4ug0GAcKykG', '+5519989182626', 'gestor'),
         (3, 'André Neder', 'admin@extramaquinassp.com.br', '$2b$10$t2T0vCYkdb3mBzUh9Zw8Tui9X8v4He/4ET/BNCfR.b4ug0GAcKykG', '+5565999106432', 'gestor');`
     ),
